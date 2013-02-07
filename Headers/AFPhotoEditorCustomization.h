1742246be5cd669d94bb5f8c8c08cc9deebab51f
Aaron Wojnowski <speedyapocalypse@live.com>
2013-02-07 17:52:49 -0600

    Update AviarySDK to version 2.6.

--- Headers/AFPhotoEditorCustomization.h
+++ Headers/AFPhotoEditorCustomization.h
@@ -62,7 +62,6 @@ extern NSString *const kAFLeftNavigationTitlePresetExit;   /* Exit */
 extern NSString *const kAFRightNavigationTitlePresetDone;  /* Done */
 extern NSString *const kAFRightNavigationTitlePresetSave;  /* Save */
 extern NSString *const kAFRightNavigationTitlePresetNext;  /* Next */
-extern NSString *const kAFRightNavigationTitlePresetSend;  /* Send */
 
 extern NSString *const kAFCropPresetName;   /* Name */
 extern NSString *const kAFCropPresetWidth;  /* Width */
